31bdb302121863152221eea9678feffb67de1161
sw9487 <62645138+sw9487@users.noreply.github.com>
2024-05-11 11:57:38 +0800

    Add files via upload

---
+++ TTS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TTS : MonoBehaviour
+{
+    /// <summary>
+    /// 語音合成的api位址
+    /// </summary>
+    [SerializeField] protected string m_PostURL = string.Empty;
+    /// <summary>
+    /// 計算方法呼叫的時間
+    /// </summary>
+    [SerializeField] protected Stopwatch stopwatch = new Stopwatch();
+    /// <summary>
+    /// 語音合成，回音訊
+    /// </summary>
+    /// <param name="_msg"></param>
+    /// <param name="_callback"></param>
+    public virtual void Speak(string _msg, Action<AudioClip> _callback) { }
+    /// <summary>
+    /// 合成語音返回音頻，同時返回合成的文本
+    /// </summary>
+    /// <param name="_msg"></param>
+    /// <param name="_callback"></param>
+    public virtual void Speak(string _msg, Action<AudioClip, string> _callback) { }
+
+
+}
